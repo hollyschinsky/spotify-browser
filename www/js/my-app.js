49bf2b0a41e9061e360a00d2b8d560d85fd3a799
Holly Schinsky <hollyschinsky@gmail.com>
2016-01-26 13:33:25 -0500

    Updated

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,7 +1,6 @@
 Template7.registerHelper('stringify', function (context){
     var str = JSON.stringify(context);
-    // Need to replace any single quotes in the data with the HTML char to avoid string being cut short
-    return str.split("'").join('&#39;');
+    return str.replace(/'/g, '&#39;');
 });
 
 // Initialize your app
@@ -9,7 +8,7 @@ var myApp = new Framework7({
     precompileTemplates: true,
     template7Pages: true,
     modalTitle: "Spotify Browser"
-})
+});
 
 // Export selectors engine (jQuery like)
 var $$ = Dom7;
@@ -20,31 +19,68 @@ var mainView = myApp.addView('.view-main', {
     domCache: true
 });
 
+// Handle Range Slider
+// - This function displays the value next to the slider as it slides for better visual indicator
+$$(document).on('input change', 'input[type="range"]', function (e) {
+    $$('input#sliderVal').val(this.value);
+});
+
 // Handle the Cordova deviceready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+});
+
+// Handle Submit Button
+// - This function calls the Spotify Web API with the designated search options then loads the list
+// page when a response is received.
+$$(document).on('click', '#btnSearch', function (e) {
+    var term = $$("#term").val();
+    if (term.length==0) {
+        myApp.alert("Please enter a search term.");
+    }
+    else {
+        var mediaType = "track";
+        var numResults = $$("#numResults").val();
+
+        var url = "https://api.spotify.com/v1/search?q=" + term +"&type=" + mediaType + "&limit=" + numResults;
+
+        $$.ajax({
+            dataType: 'json',
+            url: url,
+            success: function (resp) {
+                mainView.router.load({
+                    template: Template7.templates.listTemplate,
+                    context: resp.tracks.items
+                });
+            },
+            error: function (xhr) {
+                console.log("Error on ajax call " + xhr);
+            }
+        });
+    }
 });
 
 /* Media List Page Handling */
 myApp.onPageInit('list', function (page) {
     $$(page.container).find('.preview').on('click', function (e) {
         e.stopPropagation();
-        var item = page.context[this.dataset.item]; //this.dataset.item returns data held in data-item attribute set in template
+        // this.dataset.item returns data held in data-item attribute set in template
+        var item = page.context[this.dataset.item]; 
 
         myApp.alert("Previewing " + item.name);
         var media = new Media(item.preview_url, function () {console.log("Media Success");},function (error)
             {console.log("Media fail " + error)},null);
         media.play();
-        setTimeout(function() {media.stop()},7000) //preview for 7 seconds
+        setTimeout(function() {media.stop()},7000); //preview for 7 seconds
     });
     $$(page.container).find('.favorite').on('click', function (e) {
         e.stopPropagation();
-        var item = page.context[this.dataset.item]; //this.dataset.item returns data held in data-item attribute set in template
+        var item = page.context[this.dataset.item]; 
         myApp.alert(item.name + ' added to favorites!');
     });
     $$(page.container).find('.share').on('click', function (e) {
         e.stopPropagation();
-        var item = page.context[this.dataset.item]; //this.dataset.item returns data held in data-item attribute set in template
+        var item = page.context[this.dataset.item]; 
 
         if (window.plugins && window.plugins.socialsharing) {
             window.plugins.socialsharing.share("Hey! My new favorite song is " + item.name + " check it out!",
@@ -83,48 +119,6 @@ myApp.onPageInit('media', function (page) {
     });
 });
 
-/*
-    Handle Range Slider
-    - This function displays the value next to the slider as it slides for better visual indicator
-*/
-$$(document).on('input change', 'input[type="range"]', function (e) {
-    $$('input#sliderVal').val(this.value);
-})
-
-/*
-    Handle Submit Button
-    - This function calls the Spotify Web API with the designated search options then loads the list
-      page when a response is received.
-*/
-$$(document).on('click', '#btnSearch', function (e) {
-    var term = $$("#term").val();
-    if (term.length==0) {
-        myApp.alert("Please enter a search term.");
-    }
-    else {
-        var mediaType = "track";
-        var numResults = $$("#numResults").val()
-
-        var url = "https://api.spotify.com/v1/search?q=" + term +"&type=" + mediaType + "&limit=" + numResults;
-        var url2  = encodeURI(url,"UTF-8");
-
-        $$.ajax({
-            dataType: 'json',
-            url: url2,
-            success: function (resp) {
-                 //myApp.template7Data = resp.tracks.items;
-                 mainView.router.load({
-                    template: Template7.templates.listTemplate,
-                    //context: myApp.template7Data
-                     context: resp.tracks.items
-                });
-            },
-            error: function (xhr) {
-                console.log("Error on ajax call " + xhr);
-            }
-        });
-    }
-})
 
 /* Side Menu Handlers */
 $$(document).on('click', '#favorites', function (e) {
