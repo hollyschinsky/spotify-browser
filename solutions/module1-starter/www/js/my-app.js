04c86ded80c47927af07c3bbecdb01841b463069
Holly Schinsky <hollyschinsky@gmail.com>
2016-01-27 09:18:12 -0700

    Updated starter project

--- solutions/module1-starter/www/js/my-app.js
+++ solutions/module1-starter/www/js/my-app.js
@@ -1,25 +1,14 @@
-// Initialize your app
-var myApp = new Framework7({
-    precompileTemplates: true,
-    template7Pages: true,
-    modalTitle: "Spotify Browser"
-});
+// Initialize app
+var myApp = new Framework7();
 
-// Export selectors engine (jQuery like)
+
+// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add views - this app uses only one view stack
+// Add view
 var mainView = myApp.addView('.view-main', {
-    dynamicNavbar: true,
-    domCache: true
-});
-
-/*
-    Handle Range Slider
-    - This function displays the value next to the slider as it slides for better visual indicator
-*/
-$$(document).on('input change', 'input[type="range"]', function (e) {
-    $$('input#sliderVal').val(this.value);
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
 });
 
 // Handle Cordova Device Ready Event
@@ -27,3 +16,28 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
